8477ad36b725a0a6a68bb8f9ee559affde53db04
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-11-09 16:06:04 -0300

    feat: aula03

---
+++ 3-operadores-aritmeticos/operadores_aritimeticos.js
@@ -0,0 +1,34 @@
+// > Operadores aritmeticos
+
+let numero1 = 20;
+let numero2 = 2;
+
+// Adicao:           +
+// Subtracao         -
+// Multiplicacao     *
+// Divisao           /
+// Exponenciacao     **
+// Resto da divisao %
+
+console.log("=====================================")
+console.log("Operadores aritmeticos")
+const soma = numero1 + numero2;
+
+console.log("Soma = ", soma)
+console.log("Subtracao = ", numero1 - numero2)
+console.log("Multiplicacao = ",  numero1 * numero2)
+console.log("Divisao = ", numero1 / numero2)
+console.log("Exponenciacao = ", numero1 ** numero2)
+console.log("Resto da divisao = ", soma % 2)
+
+console.log("=====================================")
+console.log("Precedencia de operadores")
+// Precedencia de operadores
+
+console.log(3 + 7 * 3);
+console.log((10 + 7 * 8) / 3);
+
+console.log("=====================================")
+console.log("Biblioteca Math")
+// Biblioteca Math
+console.log(Math.PI)
