e4f93b3f6081746deed557a59fc56f47a9650e17
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-11-09 15:28:24 -0300

    feat: aula02

---
+++ 2-variaveis-operadores/variaveis.js
@@ -0,0 +1,34 @@
+// Variáveis
+
+let idade = 20 // Criando a primeira variável
+
+//console.log(idade)
+
+// Tipos de variáveis
+
+idade = 30;              // Number
+let numero = 30         //  Number
+let altura = 1.77      //   Number
+let nome = "Maria"    //    String (Texto)
+let estudando = true //     Booleano (true ou false)
+let outra; // Nao definida
+let nula = null;
+const constante = 100;
+
+constante = 20;
+
+// Imprimirs os valores das variáveis.
+console.log("==========================")
+console.log("Numero: " + numero);
+console.log("Altura: " + altura);
+console.log("Nome: " + nome);
+console.log("Estudando" + estudando);
+
+// Verificar o tipo da varável
+console.log("==========================")
+console.log("A variável Numero é do tipo: " + typeof numero);
+console.log("A variável Altura é do tipo: " + typeof altura);
+console.log("A variável Nome é do tipo: " + typeof nome);
+console.log("A variável Estudando é do tipo: " + typeof estudando);
+console.log("A variável Outa é do tipo: " + typeof outra);
+console.log("A variável Nula é do tipo: " + typeof nulla);
